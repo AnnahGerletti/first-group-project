87936c3111aea513d3da5be74f49d36cbba40de8
sherlyc <sherlyc@gmail.com>
2017-07-14 10:15:48 +1200

    in process post route

--- routes.js
+++ routes.js
@@ -8,6 +8,15 @@ router.get('/ptp', function(req, res) {
   res.render('postit/index.hbs', data)
 })
 
+router.get('/ptp/:id', function(req, res){
+  var id = req.params.id
+  var singlePostit = data.postit.find(function(postitNote){
+    return id == singlePostit.id
+  })
+  res.render('postit/edit', singlePostit)
+})
 
+router.post('/ptp/edit/:id', function(req, res){
+})
 
 module.exports = router
