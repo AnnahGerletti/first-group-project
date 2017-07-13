b649aa1e2bf8b60d819b2d662a7b2afc016b7b74
sherlyc <sherlyc@gmail.com>
2017-07-14 11:32:07 +1200

    got the post working

--- routes.js
+++ routes.js
@@ -8,6 +8,7 @@ router.get('/ptp', function(req, res) {
   res.render('postit/index.hbs', data)
 })
 
+//main pg
 router.get('/ptp/:id', function(req, res){
   var id = req.params.id
   var singlePostit = data.postit.find(function(postitNote){
@@ -16,7 +17,35 @@ router.get('/ptp/:id', function(req, res){
   res.render('postit/edit', singlePostit)
 })
 
+//edit page without edit functionality
+router.get('/ptp/edit/:id', function(req, res) {
+  var id = req.params.id
+
+  fs.readFile(__dirname + '/data.json', 'utf8', (err, data) => {
+    if (err) throw err
+    var singlePostit = JSON.parse(data).postit.find(function(singlePostit){
+      return id == singlePostit.id
+    })
+    res.render('postit/edit', singlePostit)
+  })
+})
+
+
+
+//adds edit power
 router.post('/ptp/edit/:id', function(req, res){
+  var postitData = req.body
+  var id = req.params.id
+  var singlePostit = data.postit.find(function(singlePostit) {
+    return id == singlePostit.id
+  })
+
+  singlePostit.comment = postitData.comment
+
+  fs.writeFile('data.json', JSON.stringify(data), function(err){
+    if (err) throw err
+    res.redirect('/ptp')
+  })
 })
 
 
